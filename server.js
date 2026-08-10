31684658070cafe97e819ba02c5b92b2022762da
wahidulsami <wahidulislamsami5@gmail.com>
2026-08-11 00:32:13 +0600

    chore: set up Prisma client and integrate user creation endpoint

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import prisma from "./src/config/db.js";
 
 const app = express();
 
@@ -11,6 +12,39 @@ app.get("/", (req, res) => {
     success: true,
     message: "Express server is running 🚀",
   });
+});
+
+
+app.post("/api/v1/users", async (req, res) => {
+  try {
+    const { name, email } = req.body;
+
+    if (!name || !email) {
+      return res.status(400).json({
+        success: false,
+        message: "Name and email are required",
+      });
+    }
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+      },
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      data: user,
+    });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
 });
 
 app.listen(PORT, () => {
